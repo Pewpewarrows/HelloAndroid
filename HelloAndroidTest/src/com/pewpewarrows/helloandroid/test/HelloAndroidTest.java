fb4ba6f981baf04888fbfe8d03d4e2dfc9bc1067
Marco Chomut <marco.chomut@gmail.com>
2011-10-20 11:49:45 -0400

    Initial commit, project structure for core app and test runner

---
+++ HelloAndroidTest/src/com/pewpewarrows/helloandroid/test/HelloAndroidTest.java
@@ -0,0 +1,33 @@
+package com.pewpewarrows.helloandroid.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.pewpewarrows.helloandroid.HelloAndroid;
+
+public class HelloAndroidTest extends
+		ActivityInstrumentationTestCase2<HelloAndroid> {
+
+	private HelloAndroid mActivity;
+	private TextView mView;
+	private String resourceString;
+
+	public HelloAndroidTest() {
+		super("com.pewpewarrows.helloandroid", HelloAndroid.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = this.getActivity();
+		mView = (TextView) mActivity
+				.findViewById(com.pewpewarrows.helloandroid.R.id.textview);
+		resourceString = mActivity
+				.getString(com.pewpewarrows.helloandroid.R.string.hello);
+	}
+
+	public void testPreconditions() {
+		assertNotNull(mView);
+	}
+
+}
